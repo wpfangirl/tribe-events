22a68214bd489351b9ba240aa4845361f18a3c99
Sallie Goetsch <sallie@wpfangirl.com>
2016-06-28 16:35:54 -0700

    initial project version

---
+++ default-template.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Default Events Template
+ * This file is the basic wrapper template for all the views if 'Default Events Template'
+ * is selected in Events -> Settings -> Template -> Events Template.
+ *
+ * Override this template in your own theme by creating a file at [your-theme]/tribe-events/default-template.php
+ *
+ * @package TribeEventsCalendar
+ *
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die( '-1' );
+}
+// Set layout to full-width
+add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
+
+// Remove breadcrumbs
+remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs' );
+
+// Remove Genesis Loop 
+remove_action( 'genesis_loop', 'genesis_do_loop' );
+
+// Add Events Calendar Page Template
+add_action('genesis_loop','genesis_tribe');
+function genesis_tribe(){
+?>
+<div id="tribe-events-pg-template">
+	<?php tribe_events_before_html(); ?>
+	<?php tribe_get_view(); ?>
+	<?php tribe_events_after_html(); ?>
+</div> <!-- #tribe-events-pg-template -->
+<?php
+}
+
+genesis();
